c98548264ed39447b9550dd6fed6a1e0f59bd9c8
avvessalom <avvessalom@gmail.com>
2021-05-13 16:06:22 +0300

    init structs

--- kernel/memory/paging.c
+++ kernel/memory/paging.c
@@ -2,3 +2,14 @@
 // Created by Lazurin Eugene on 13.05.2021.
 //
 
+#include <inttypes.h>
+#include "paging.h"
+
+#define PAGE_SIZE 4096
+#define PAGE_COUNT PAGE_SIZE / 32
+extern int8_t endkernel;
+struct page_table_entry_t pageDirectory[PAGE_COUNT];
+
+void set_page_table_entry(struct page_table_entry_t *table, int index) {
+
+}
---
+++ kernel/memory/paging.h
@@ -0,0 +1,27 @@
+//
+// Created by kain on 13.05.2021.
+//
+
+#ifndef MEMOS_PAGING_H
+#define MEMOS_PAGING_H
+
+
+struct page_table_entry_t {
+    char phys_page_adress: 1;
+    char avail: 3;
+    char global: 1;
+    char zero: 1;
+    char dirty: 1;
+    char accessed: 1;
+    char cache_disabled: 1;
+    char write_throught: 1;
+    char user_supervisor: 1;
+    char read_write: 1;
+    char present: 1;
+}__attribute((__packed__));;
+
+
+void set_page_table_entry(struct page_table_entry_t *table, int index);
+
+
+#endif //MEMOS_PAGING_H
