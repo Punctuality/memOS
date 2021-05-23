2417efbc44c3c39065ba97c26bc92e054315e4ae
avvessalom <avvessalom@gmail.com>
2021-05-23 14:37:41 +0300

    change memory init from hardcode to structs

--- kernel/memory/i_hate_paging.c
+++ kernel/memory/i_hate_paging.c
@@ -5,24 +5,29 @@
 #include <stdint.h>
 #include "include/paging.h"
 
-uint32_t page_directory[1024] __attribute__((aligned(4096)));
-uint32_t first_page_table[1024] __attribute__((aligned(4096)));
+struct page_directory_entry_t pageDirectoryEntry[1024] __attribute__((aligned(4096)));
+struct page_table_entry_t first_page_table[1024] __attribute__((aligned(4096)));
 
 extern void loadPageDirectory(unsigned int*);
 extern void enablePaging();
 
 void set_page_dir() {
     for (int i = 0; i < 1024; i++) {
-        page_directory[i] = 0x00000002;
+        pageDirectoryEntry[i].read_write = 1;
     }
     for (unsigned int i = 0; i < 1024; i++) {
-        first_page_table[i] = (i * 0x1000) | 3;
+        first_page_table[i].phys_page_address = i * 0x0001;
+        first_page_table[i].read_write = 1;
+        first_page_table[i].present = 1;
     }
-    page_directory[0] = ((uint32_t)first_page_table) | 3;
 
-    loadPageDirectory(page_directory);
+    pageDirectoryEntry[0].page_table_4kb_aligned_address = ((unsigned int) &first_page_table) >> 12 & 0x3FF; // attention HARDCODE
+    pageDirectoryEntry[0].read_write = 1;
+    pageDirectoryEntry[0].present = 1;
+
+    loadPageDirectory(pageDirectoryEntry);
 
     enablePaging();
 
-    for(;;);
+//    for(;;);
 }
--- kernel/memory/include/paging.h
+++ kernel/memory/include/paging.h
@@ -7,4 +7,32 @@
 
 void set_page_dir();
 
+struct page_directory_entry_t {
+    unsigned char present: 1;
+    unsigned char read_write: 1;
+    unsigned char user_supervisor: 1;
+    unsigned char write_through: 1;
+    unsigned char cache_disabled: 1;
+    unsigned char accessed: 1;
+    unsigned char zero: 1;
+    unsigned char page_size: 1; // 0 or 4kb
+    unsigned char ignored: 1;
+    unsigned char avall: 3;
+    unsigned int page_table_4kb_aligned_address: 20;
+} __attribute((__packed__));
+
+struct page_table_entry_t {
+    unsigned char present: 1;
+    unsigned char read_write: 1;
+    unsigned char user_supervisor: 1;
+    unsigned char write_through: 1;
+    unsigned char cache_disabled: 1;
+    unsigned char accessed: 1;
+    unsigned char dirty: 1;
+    unsigned char zero: 1;
+    unsigned char global: 1;
+    unsigned char avail: 3;
+    unsigned int phys_page_address: 20;
+}__attribute((__packed__));
+
 #endif //MEMOS_PAGING_H
