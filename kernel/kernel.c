d12aae59e8c30a19762cb3fe629cbd08337fe5e1
avvessalom <avvessalom@gmail.com>
2021-06-14 14:17:20 +0300

    update timer

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,28 +2,37 @@
 #include "../drivers/descriptor_tables.h"
 #include "shell.h"
 #include "memory/include/paging.h"
-#include "util.h"
 #include "multiboot.h"
+#include "threading/include/task.h"
+#include "../drivers/timer.h"
 #include <stdint.h>
 
 
 extern void keyboard_handler_int();
 
-
-void initialize(){
-
-    init_descriptor_tables();
-    load_idt_entry(0x21, (unsigned long) keyboard_handler_int, 0x08, 0x8e);
-    kb_init();
-    set_page_dir();
-    shell_init();
-}
+unsigned int initial_esp;
 
 void kmain(struct multiboot_info *info, uint32_t initialStack) {
 
-    initialize();
+    initial_esp = initialStack;
+
+    init_descriptor_tables();
+
+    load_idt_entry(0x21, (unsigned long) keyboard_handler_int, 0x08, 0x8e);
+    kb_init();
+
+    set_page_dir();
+
+    tasking_init();
+
+    shell_init();
+
     asm volatile ("int $0x0");
     asm volatile ("int $0x1");
     asm volatile ("int $0x4");
+
+    asm volatile("sti");
+    init_timer(50);
+
     while(1) __asm__("hlt\n\t");
 }
